b17f1229a7631a4b2c3465f33ace95ebde153e14
Jason Gutierrez <jasongeee@yahoo.com>
2022-10-22 23:22:35 -0700

    structured sections for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,35 @@ import styles from "../styles/Home.module.css";
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <h1 className="text-lg">Jason Gutierrez</h1>
+      <header>
+        {/* First Section */}
+        <div>
+          <h1 className="text-lg">Jason Gutierrez</h1>
+        </div>
+        {/* Second Section */}
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Resume</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </header>
+      <main>
+        {/* Main Section */}
+        <div></div>
+        {/* About Section */}
+        <div></div>
+        {/* Skils Section */}
+        <div></div>
+        {/* Projects Section */}
+        <div></div>
+        {/* Contact Form Section */}
+        <div></div>
+      </main>
     </div>
   );
 };
